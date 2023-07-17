6ec89778dfd479b3593524a38d4408c8b63f6960
Kerim111 <kerimalp3407@gmail.com>
2023-07-17 03:04:16 +0300

    oyun bitti

--- U-151 2D/Assets/portalgun.cs
+++ U-151 2D/Assets/portalgun.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst.Intrinsics;
-using Unity.PlasticSCM.Editor.WebApi;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -30,35 +27,45 @@ public class portalgun : MonoBehaviour
         Vector3 aimdirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.parent.position).normalized;
         float angle = Mathf.Atan2(aimdirection.y, aimdirection.x) * Mathf.Rad2Deg;
         transform.parent.eulerAngles = new Vector3(0, 0, angle);
-        line.SetPosition(0,tip.transform.position);
-        line.SetPosition(1, Physics2D.Raycast(origin: gameObject.transform.parent.position, direction: gameObject.transform.right, distance: 1000,layer).point);
-        if (Input.GetButtonDown("Fire1")) 
+        line.SetPosition(0, tip.transform.position);
+        line.SetPosition(1, Physics2D.Raycast(origin: gameObject.transform.parent.position, direction: gameObject.transform.right, distance: 1000, layer).point);
+        if (Input.GetButtonDown("Fire1"))
         {
-            RaycastHit2D hit = Physics2D.Raycast(origin:gameObject.transform.parent.position,direction:gameObject.transform.right,distance:Mathf.Infinity,layerMask:LayerMask.GetMask("ground"));
-            if(hit != null && hit.collider != null) 
+            RaycastHit2D hit = Physics2D.Raycast(origin: gameObject.transform.parent.position, direction: gameObject.transform.right, distance: Mathf.Infinity, layerMask: LayerMask.GetMask("ground"));
+            Debug.DrawLine(hit.point, hit.point + Vector2.right, Color.red,100);
+            if (hit != null && hit.collider != null)
             {
-                if(current== null) 
+                if (!Physics2D.OverlapCircle(hit.point, 5, LayerMask.GetMask("portal")))
                 {
-                    current = GameObject.Instantiate(Portal);
-                    current.transform.GetChild(0).transform.position = hit.point;
-                    current.transform.GetChild(0).rotation = Quaternion.FromToRotation(Vector2.right, hit.normal);
-                    color = Random.ColorHSV();
-                    color.a = 0.5f;
-                    current.transform.GetChild(0).GetComponent<SpriteRenderer>().color = color;
+                    if (current == null)
+                    {
+                        current = GameObject.Instantiate(Portal);
+                        current.transform.GetChild(0).transform.position = hit.point;
+                        current.transform.GetChild(0).rotation = Quaternion.FromToRotation(Vector2.right, hit.normal);
+                        color = Random.ColorHSV();
+                        color.a = 0.5f;
+                        current.transform.GetChild(0).GetComponent<SpriteRenderer>().color = color;
+                    }
+                    else
+                    {
+                        current.transform.GetChild(1).gameObject.SetActive(true);
+                        current.transform.GetChild(1).transform.position = hit.point;
+                        current.transform.GetChild(1).rotation = Quaternion.FromToRotation(Vector2.right, hit.normal);
+                        current.GetComponent<portalparent>().activated = true;
+                        color.a = 1;
+                        current.transform.GetChild(1).GetComponent<SpriteRenderer>().color = color;
+                        current.transform.GetChild(0).GetComponent<SpriteRenderer>().color = color;
+                        current = null;
+                    }
                 }
-                else 
-                {
-                    current.transform.GetChild(1).gameObject.SetActive(true);
-                    current.transform.GetChild(1).transform.position = hit.point;
-                    current.transform.GetChild(1).rotation = Quaternion.FromToRotation(Vector2.right, hit.normal);
-                    current.GetComponent<portalparent>().activated= true;
-                    color.a = 1;
-                    current.transform.GetChild(1).GetComponent<SpriteRenderer>().color = color;
-                    current.transform.GetChild(0).GetComponent<SpriteRenderer>().color = color;
-                    current = null;
-                }
-            }    
-           
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (current != null)
+            {
+                Destroy(current); current = null;
+            }
         }
     }
 }
